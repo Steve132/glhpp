45594663b324bda832d4bc1ec68e3b7edc8a4571
Steve132 <steve@soapforge.com>
2020-08-24 17:29:50 -0400

    GLHPP GEARS

---
+++ test/glhppgears.cpp
@@ -0,0 +1,13 @@
+#define GL_GLEXT_PROTOTYPES
+#include "legacy/glcorearb.h"
+#include "glhpp/OpenGL.hpp"
+#include<iostream>
+#include<utility>
+
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+	
+	return 0;
+}
